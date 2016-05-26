7301c2da40f9630f918c0c045ca6563e53f84389
Krishnan Rajiyah <Krajiyah@gmail.com>
2016-05-26 01:32:16 -0400

    Update FirebaseUtils.java

--- firebase/FirebaseUtils.java
+++ firebase/FirebaseUtils.java
@@ -1,35 +1,54 @@
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Base64;
-import android.util.Log;
-import android.widget.ImageView;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+import android.telecom.Call;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import org.mobiledevsberkeley.dealonconcept.objects.Deal;
+import org.mobiledevsberkeley.dealonconcept.objects.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
- * Created by krishnan on 5/16/2016.
+ * Created by krishnan on 4/19/2016.
  */
 public class FirebaseUtils {
-    public static final Firebase ref = new Firebase("https://<name of firebase app>.firebaseio.com/");
+    public static final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    public static FirebaseUser curr = FirebaseUser.getInstance().getCurrentUser();
 
-    public static void setImageViewByFirebaseImageUrlAsync(final ImageView imageView, String url) {
-        ref.child(url).addValueEventListener(new ValueEventListener() {
+    public static Callable<Void> chainQuery(final Query q, final Class objectClass, final List storeResults, final Callable<Void> callback) {
+        return new Callable<Void>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String binStr = dataSnapshot.getValue(String.class);
-                if(binStr.contains("jpeg")) binStr = binStr.replace("data:image/jpeg;base64,","");
-                else if (binStr.contains("png")) binStr = binStr.replace("data:image/png;base64,","");
-                byte[] decodedString = Base64.decode(binStr, Base64.DEFAULT);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-                imageView.setImageBitmap(bitmap);
+            public Void call() throws Exception {
+                q.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot result: dataSnapshot.getChildren()) {
+                            storeResults.add(result.getValue(objectClass));
+                        }
+                        try {
+                            if(callback != null) callback.call();
+                        } catch (Exception e) {
+                            Utils.logStackTrace(e);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Utils.logStackTrace(databaseError.toException());
+                    }
+                });
             }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                // handle error here
-            }
-        });
+        };
     }
 }
